2849270dc327f1e79a07e841f652eb6af2698f5a
Etieno Andem <etienoandem@gmail.com>
2022-09-14 00:56:22 -0700

    fix it felix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,14 +2,14 @@
 /**
  * print_last_digit -> prints the last digit
  * @n: the passed args
- * Return: the last digit
+ * Return: integer value
  */
 int print_last_digit(int n)
 {
 	int j;
 
 	if (n < 0)
-		n = -n;
+		n = n * -1;
 	j = n % 10;
 	_putchar(j + '0');
 	return (j);

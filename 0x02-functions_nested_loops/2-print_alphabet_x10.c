e77ad632d92fec0504c6ec1f21d833f1a48eb5f4
Etieno Andem <etienoandem@gmail.com>
2022-09-13 17:01:12 -0700

    2-print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet x10
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+	int alphabet;
+	int alphabet2;
+
+	for (alphabet = 0; alphabet <= 9; alphabet++)
+	{
+		for (alphabet2 = 'a'; alphabet <= 'z'; alphabet2++)
+		{
+			_putchar(alphabet2);
+		}
+		_putchar(alphabet);
+	}
+}

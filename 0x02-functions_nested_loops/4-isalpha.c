16f7ce0ea33d29b7c193b96bdcef003c6513e96d
Etieno Andem <etienoandem@gmail.com>
2022-09-13 20:46:42 -0700

    4 is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _isalpha(int c) - checks for alphabetic character
+ * _isalpha - checks for alphabetic character
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */

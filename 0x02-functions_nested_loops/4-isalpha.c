5103f8eb5c9247f5b7f32c42e23dc941613259ce
Etieno Andem <etienoandem@gmail.com>
2022-09-13 22:12:00 -0700

    hot fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
  * _isalpha - checks if the letter is lower case
- * @c : character to check
- * Return:0 or 1
+ * @c : character to be checked on
+ * Return:retutn 0 or 1 depending on the condition
  */
 
 int _isalpha(int c)
 
 {
 
-	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 
 }

0045c55696b0e460a444a1d58d550cc017c07453
Etieno Andem <etienoandem@gmail.com>
2022-09-13 21:31:48 -0700

    Hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,18 @@
  *
  * Return: Always 0 (Success)
  */
+
 void print_alphabet_x10(void)
 {
-	int x;
-	int y;
+	int alpha;
+	int alpha2;
 
-	for (x = 0; x <= 10; x++)
+	for (alpha = 0; alpha < 10: alpha++)
 	{
-		for (y = 'a'; y <= 'z'; y++)
+		for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
 		{
-			_putchar(y);
+			_putchar(alpha2);
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }

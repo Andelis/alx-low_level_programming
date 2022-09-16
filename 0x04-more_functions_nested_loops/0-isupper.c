8d1ed66ee2a5b7f8a7722ba759425537b7a34cb7
Etieno Andem <etienoandem@gmail.com>
2022-09-16 07:36:46 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper -> this is a function to check uppercase character
+ *
+ *@c:the charater putchar
+ *
+ *Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	}
+		return (1);
+	}
+	return (0);
+}

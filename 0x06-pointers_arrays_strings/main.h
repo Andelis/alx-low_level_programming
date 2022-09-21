9d50214f3521ad5de4bc551a0c6db31ba5e21d97
Etieno Andem <etienoandem@gmail.com>
2022-09-21 10:42:18 -0700

    fix?

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
@@ -8,4 +9,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+
 #endif

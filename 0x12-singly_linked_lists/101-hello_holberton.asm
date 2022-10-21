cac4cfccc9bab9adb8c0f1914273b5b5c7b8a557
Etieno Andem <etienoandem@gmail.com>
2022-10-20 21:18:09 -0700

    double checked

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,9 @@
-global main
-
-	section .text
-
+          global    main
+          extern    printf
 main:
-	mov	rax, 1
-	mov	rdi, 1
-	mov	rsi, m
-	mov	rdx, 17
-	syscall
-
-m:
-	db "Hello, Holberton\n", 10, 0
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
